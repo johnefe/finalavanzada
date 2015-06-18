8a2ed6ddd8557dbee8b79b7844d900e42bdf5252
johnefe <pc_systemjf@hotmail.com>
2015-06-17 22:25:14 -0500

    se sube proyecto final
    se sube proyecto final

---
+++ tienda3/src/java/com/tienda2/bean/VentaProductoFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tienda2.bean;
+
+import com.tienda2.entidades.VentaProducto;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author jhonf
+ */
+@Stateless
+public class VentaProductoFacade extends AbstractFacade<VentaProducto> {
+    @PersistenceContext(unitName = "tienda2PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public VentaProductoFacade() {
+        super(VentaProducto.class);
+    }
+    
+}
---
+++ tienda3/src/java/com/tienda2/entidades/Ventas.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tienda2.entidades;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author jhonf
+ */
+@Entity
+@Table(name = "ventas")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Ventas.findAll", query = "SELECT v FROM Ventas v"),
+    @NamedQuery(name = "Ventas.findById", query = "SELECT v FROM Ventas v WHERE v.id = :id"),
+    @NamedQuery(name = "Ventas.findByFecha", query = "SELECT v FROM Ventas v WHERE v.fecha = :fecha")})
+public class Ventas implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "fecha")
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "ventas")
+    private Collection<VentaProducto> ventaProductoCollection;
+    @JoinColumn(name = "id_cliente", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Cliente idCliente;
+
+    public Ventas() {
+    }
+
+    public Ventas(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    @XmlTransient
+    public Collection<VentaProducto> getVentaProductoCollection() {
+        return ventaProductoCollection;
+    }
+
+    public void setVentaProductoCollection(Collection<VentaProducto> ventaProductoCollection) {
+        this.ventaProductoCollection = ventaProductoCollection;
+    }
+
+    public Cliente getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Cliente idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Ventas)) {
+            return false;
+        }
+        Ventas other = (Ventas) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.tienda2.entidades.Ventas[ id=" + id + " ]";
+    }
+    
+}
